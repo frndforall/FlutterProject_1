c786d9bd5982d2c8eab7fe351e834afbec3b16ea
Nagendra sharma <nagendra.sharma@cybersoft.net>
2020-05-09 13:02:29 +0530

    Home Screen Content

--- lib/src/screens/meetup_home_screen.dart
+++ lib/src/screens/meetup_home_screen.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MeetUpHomeScreen extends StatefulWidget {
@@ -10,18 +12,53 @@ class MeetUpHomeScreen extends StatefulWidget {
   
 }
 
-class MeetupHome extends State{
+class MeetupHome extends State<MeetUpHomeScreen>{
+  List<CustomText> customList =[CustomText(key: ValueKey('1')),CustomText(key: ValueKey('2')),CustomText(key: ValueKey('3'))];
+
+  shuffleList() {
+    setState(() {
+      customList.shuffle();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Column(
-      mainAxisAlignment:MainAxisAlignment.center,
-      children: <Widget>[
-      Text('I am meetup home scree',textDirection: TextDirection.ltr),
-      Text('I am the second line in the screen',textAlign: TextAlign.right,)
-    ],)
-    ),
-    appBar: AppBar(title: Text('Meetup Home'))
+    return Scaffold(body: Column(children: customList),
+    appBar: AppBar(title: Text('Meetup Home')),
+    floatingActionButton: FloatingActionButton(onPressed: () {
+      shuffleList();
+    },
+    tooltip: 'Shuffle List'),
     );
   }
 
+}
+
+class CustomText extends StatefulWidget {
+
+  CustomText({Key key}) : super(key: key);
+  @override
+  CustomTextState createState() => CustomTextState();
+  
+}
+
+class CustomTextState extends State<CustomText> {
+  final List colors = [Colors.red,Colors.blue,Colors.black,Colors.brown,Colors.grey];
+  Random random = Random();
+  Color color;
+
+
+  @override
+  void initState() {
+    color = colors[random.nextInt(colors.length)];
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Displaying Text for color $color'),
+      color: this.color,
+      width: 150,
+      );
+  }
+  
 }
